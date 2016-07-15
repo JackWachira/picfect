9996c33af798ff19d846dc43c4cabc33f3d8867f
Jack Mwangi <JackMwangi@Jack.local>
2016-07-15 11:08:54 +0300

    (Chore) Add static url

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -136,6 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Django Allauth
 SITE_ID = 1
